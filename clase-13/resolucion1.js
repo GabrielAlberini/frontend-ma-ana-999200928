70d97746fc0a985fb113489c8d1167f3384da345
gabrielalberini <gabialberini733@gmail.com>
2025-12-23 11:59:11 -0300

    resolucion ej 1

---
+++ clase-13/resolucion1.js
@@ -0,0 +1,7 @@
+const edades = [18, 20, 23, 25]
+
+if (edades[0] >= 18 && edades[1] >= 18 && edades[2] >= 18 && edades[3] >= 18) {
+  console.log("Todos son mayores")
+} else {
+  console.log("Hay menores de edad")
+}
